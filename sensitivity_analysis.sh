7bafa614403552085dfdbfe15f4965648b1e6051
Naeem-Paeedeh <naeem.paeedeh@mymail.unisa.edu.au>
2024-03-14 13:16:32 +1030

    Sensitivity analysis

---
+++ sensitivity_analysis.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# TODO: Uncomment and run the python scripts for each experiment
+#--------------------------------------------------
+# Experiments:
+
+## AdamW
+
+python main.py --settings_file "Experiments/sensitivity Analysis/AdamW/phase=3,seed=1,lr1=1e-3,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/AdamW/phase=3,seed=1,lr1=1e-3,lr2=1e-3.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/AdamW/phase=3,seed=1,lr1=1e-3,lr2=1e-2.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/AdamW/phase=3,seed=1,lr1=1e-2,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/AdamW/phase=3,seed=1,lr1=1e-2,lr2=1e-2.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/AdamW/phase=3,seed=1,lr1=1e-1,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/AdamW/phase=3,seed=1,lr1=1e-1,lr2=1e-3.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/AdamW/phase=3,seed=1,lr1=1e-1,lr2=1e-2.toml"
+
+## Adam
+
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-4,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-4,lr2=1e-3.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-4,lr2=1e-2.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-3,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-3,lr2=1e-3.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-3,lr2=1e-2.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-2,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-2,lr2=1e-3.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/Adam/phase=3,seed=1,lr1=1e-2,lr2=1e-2.toml"
+
+## SGD
+
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-4,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-4,lr2=1e-3.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-4,lr2=1e-2.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-3,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-3,lr2=1e-3.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-3,lr2=1e-2.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-2,lr2=1e-4.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-2,lr2=1e-3.toml"
+# python main.py --settings_file "Experiments/sensitivity Analysis/SGD/phase=3,seed=1,lr1=1e-2,lr2=1e-2.toml"
